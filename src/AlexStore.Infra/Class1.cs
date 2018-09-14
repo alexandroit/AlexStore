4c6d5e696df03abb6c373ac94a234b9b434d10f4
Alexandro Marques <aleinfoit@gmail.com>
2018-09-14 18:10:51 -0400

    Start project demo

---
+++ src/AlexStore.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AlexStore.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/AlexStore.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AlexStore.Shared
+{
+    public class Class1
+    {
+    }
+}
